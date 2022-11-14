3de8156386ec4681b61b8c81893f58401543e51c
JoannaGoj <joannagojan@gmail.com>
2022-11-14 06:47:00 +0100

    Prework to JS React Course: Arrays

--- JS/myJScodenotes.js
+++ JS/myJScodenotes.js
@@ -78,3 +78,113 @@ let rockPaperScissors = () => {
 
 rockPaperScissors()
 
+
+// Create an array with numbers and calculate an arithmetic average of the numbers inside the array
+
+const numbers = [1,2,3,4,5,6,7]
+let arithmetic_average = (numbers) => {
+    let sum_number = 0;
+    let number_counter = 0;
+    numbers.forEach(function (number) {
+        sum_number += number;
+        number_counter++;
+    });
+    let arithmetic_average_result = sum_number / number_counter;
+    return arithmetic_average_result;
+};
+
+
+console.log(arithmetic_average(numbers));
+
+// Create an array containing names of cities, return and console.log array with the letter count of every city
+//e.g.
+// ```JavaScript
+// const cities = ["Kraków", "Olsztyn", "Szczecin", "Ostrów Wielkopolski"];
+// ```
+// The result
+//
+// ```JavaScript
+// [6, 7, 8, 19];
+// ```
+
+const cities = ["Canggu", "Bangkok", "Phuket", "Ubud", "Koh Phangan"]
+console.log(cities);
+const letterCounter = (citiesArray) => {
+    console.log(cities);
+    return citiesArray.map(function (city) {
+            let characterCounter = 0;
+            for (let character of city) {
+                if (character !== " ") {
+                    characterCounter++;
+                }
+            }
+            return characterCounter
+        }
+    )
+};
+console.log(letterCounter(cities));
+
+// Write a function that will return only negative numbers from an array
+
+
+const numbers = [4, -5, 0, 2, -67, 8, 10, -34 ];
+const getNegativeNumbers = (function (numbers){
+    const onlyNegative = numbers.filter(function (number) {
+        return Math.abs(number) !== number;
+    })
+    return onlyNegative
+});
+
+console.log(getNegativeNumbers(numbers));
+
+
+// Based on years in an array, calculated the ages
+
+const years = [1995, 1856, 2014, 1987, 2022];
+const calculateAge = (years) => {
+    const calculatedAge = years.map(function (birthYear){
+        let currentYear = new Date().getFullYear();
+        return currentYear - birthYear
+    });
+    return calculatedAge
+};
+
+console.log(calculateAge(years));
+
+
+// Create a function getSecondMaxNumber(array). The function should return the second biggest number
+// e.g.
+// ```JavaScript
+// const numbers = [2, 3, 1, 6, 100, 49, 5, 7, 8, 9 ];
+// console.log(getSecondMaxNumber(numbers));
+//
+
+const numbers = [2, 3, 50, 1, 6, 100, 49, 5, 7, 8, 9 ];
+const getSecondMaxNumber = (array) => {
+   const sortArraySmallestToBiggest = array.sort(function (a,b) {
+        return a - b
+    });
+    return array[array.length - 2];
+};
+
+console.log(getSecondMaxNumber(numbers))
+
+// Create a variable with an array "names1" containing names
+// The write in terminal second element, fifth element, length of the array
+// Define an empty array in a variable "names2", and one by one add 6 names to the array
+// Then display in the terminal the first element, the third element and the length of the array
+
+
+
+const names1 = ["Joonho", "Khoa", "Reira", "Claudio", "Adrian"]
+console.log(`Second name of first array: ${names1[1]}, Fifth name of first array: ${names1[4]}, the length of first array: ${names1.length}`);
+
+names2 = []
+names2.push("Jay");
+names2.push("Pagan");
+names2.push("Joanna");
+names2.push("Aga");
+names2.push("Syadza");
+
+
+console.log(`First name of second array: ${names2[0]}, third name of second array: ${names2[2]}, the length of second array: ${names2.length}`);
